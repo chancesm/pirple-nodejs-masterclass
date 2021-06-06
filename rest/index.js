58885bd3d70cb0b33c9a90d3b50a22ee2753e54f
Chance Murray <chance.s.murray@gmail.com>
2021-06-06 19:09:23 +0000

    get url and parse it

--- rest/index.js
+++ rest/index.js
@@ -2,10 +2,29 @@
  * This is my main entrypoint for my RESTful API
  */
 const http = require('http');
+const url = require('url');
 
 //  The server should respond to requests with a string
 const server = http.createServer((req, res) => {
+
+  // Get URL and parse it
+  // USE the URL contructor as url.parse is deprecated
+  // src: https://stackoverflow.com/questions/59375013/node-legacy-url-parse-deprecated-what-to-use-instead
+  const baseURL = req.protocol + '://' + req.headers.host + '/';
+  const parsedURL = new URL(req.url, baseURL);
+
+  // Get Path from URL
+  const path = parsedURL.pathname;
+
+  // Clean pathname (remove leading and trailing '/')
+  const trimmedPath = path.replace(/^\/+|\/+$/g, '')
+
+  // Send Response
   res.end('Hello World\n');
+
+  // Log the requested path
+  console.log(`Request received on path: ${trimmedPath}`)
+
 })
 
 // Start the server and listen on port 3000
